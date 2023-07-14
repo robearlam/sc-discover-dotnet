d54b8096d4930ac498b11df232c1f886751f0e23
Rob Earlam <robert.earlam@gmail.com>
2023-07-14 10:11:25 +1000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Testing Sitecore Discover API");
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var domainId = config.GetSection("Discover:DomainId").Value;
+var apiKey = config.GetSection("Discover:ApiKey").Value;
+
+var client = new HttpClient();
+var request = new HttpRequestMessage(HttpMethod.Post, $"https://api.rfksrv.com/search-rec/{domainId}/3");
+request.Headers.Add("Authorization", apiKey);
+
+var requestBody = @"
+    {
+        ""data"": {
+            ""context"": {
+            ""page"": {
+                ""uri"": ""/rea-home""
+            },
+            ""user"": {
+                ""new_user"": true
+            }
+            },
+            ""widget"": {
+            ""rfkid"": ""rfkid_92""
+            },
+            ""content"": {
+            ""product"": {}
+            },
+            ""force_v2_specs"": true
+        }
+}
+";
+
+var content = new StringContent(requestBody, null, "application/json");
+request.Content = content;
+
+var response = await client.SendAsync(request);
+response.EnsureSuccessStatusCode();
+
+var jsonResponse = await response.Content.ReadAsStringAsync();
+Console.WriteLine(jsonResponse);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # sc-discvover-dotnet
+This is a sample repository showing how you can call the Sitecore Discover API with C# and DotNet.
+
+# Parameters
+You will need to populate the following parameters in the `appsettings.json` file.
+
+- `DomainId`: The id of the domain in use, in the format "CompanyId-DomainId"
+- `ApiKey`: The generated API Key for the domain
+
+# Request
+The request body will need to be tailored to match the Widget definition in your Discover congifuration. 
+
+# Running the Repo
+
+Poulate the `appsettings.json` file with the parameters above, tailor the request body, then run the following commands:
+
+```C#
+dotnet restore && dotnet run
+```
